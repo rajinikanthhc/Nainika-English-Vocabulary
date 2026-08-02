1aef4377e7557d126885d15f1dce2dfb03d5526c
rajinikanthhc <150215666+rajinikanthhc@users.noreply.github.com>
2026-08-02 13:57:09 +0530

    cleanup

--- js/api.js
+++
@@ -1,7 +0,0 @@
-async function getCategories() {
-
-    const response = await fetch(CONFIG.API_URL + "?action=categories");
-
-    return await response.json();
-
-}
--- js/config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-
-    APP_NAME: "Nainika English Vocabulary",
-
-    API_URL: "https://script.google.com/macros/s/AKfycbytoH9hSXcKd7ieVoVRA9dgCglJHEPiLYL_R2XOzae0PnISy1y93u0gjbmKkUNLP5spaA/exec"
-
-};
